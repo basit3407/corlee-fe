1570a7b4ddcf407051169d84d9901e3161c1992a
basit3407 <basit3407@gmail.com>
2024-06-04 16:45:18 +0500

    testing

--- src/components/App/Navbar/ProductNavigation/index.jsx
+++ src/components/App/Navbar/ProductNavigation/index.jsx
@@ -3,10 +3,14 @@ import SvgIcon1 from "./icons/SvgIcon1";
 import "./style.css";
 import messages from "./messages.json";
 
-function ProductNavigation() {
+function ProductNavigation({ menuOpen }) {
   return (
     <div className="main-navigation-container-nav">
-      <div className="horizontal-menu-with-icons-nav">
+      <div
+        className={`horizontal-menu-with-icons-nav ${
+          menuOpen ? "menu-items show" : "menu-items"
+        }`}
+      >
         <div className="product-card-container-nav">
           <p className="contact-info-text-style-nav">{messages["products"]}</p>
           <SvgIcon1 className="svg-container-nav" />
--- src/components/App/Navbar/index.jsx
+++ src/components/App/Navbar/index.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import ProductNavigation from "./ProductNavigation";
 import "./style.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="main-content-container-nav">
       <div className="header-navbar-nav">
         <img src="/assets/logo.png" className="product-image-styles-nav" />
-        <ProductNavigation />
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <ProductNavigation menuOpen={menuOpen} />
       </div>
     </div>
   );
