13742e4dfbce5e105a171f0fee80ee62fa30bab0
basit3407 <basit3407@gmail.com>
2024-06-04 16:57:19 +0500

    tailwind setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Approuter from "./Routes/Approuter";
+import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
