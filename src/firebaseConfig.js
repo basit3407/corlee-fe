0d14e731049d261b3d54f3d9be61fb590f2dc0d4
basit3407 <basit3407@gmail.com>
2024-07-31 08:13:29 +0500

    all done. ready to launch

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,36 +1,5 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC0BK6ojLen63-w1WuOn-bRENMtyiovWR8",
-//   authDomain: "corlee-85a80.firebaseapp.com",
-//   projectId: "corlee-85a80",
-//   storageBucket: "corlee-85a80.appspot.com",
-//   messagingSenderId: "81090684417",
-//   appId: "1:81090684417:web:61c6f5abb9414067968200",
-//   measurementId: "G-G3JEN2Z8MM",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// app.languageCode = "en";
-// const provider = new GoogleAuthProvider();
-
-// export const googleLogin = async () => {
-//   const result = await signInWithPopup(auth, provider);
-//   const credential = GoogleAuthProvider.credentialFromResult(result);
-//   const token = credential.accessToken;
-//   const user = result.user;
-//   return { token, user };
-// };
-
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC0BK6ojLen63-w1WuOn-bRENMtyiovWR8",
