396a75bcb39d97309e155ac98bcdfc343b4c4de0
M-Uzair-dev <uzairmanan3@gmail.com>
2024-11-05 23:10:01 +0500

    FIxed the navbar and order process alignment issue

--- src/components/App/AboutUsScreen/ProductInfoSection/index.jsx
+++ src/components/App/AboutUsScreen/ProductInfoSection/index.jsx
@@ -5,19 +5,17 @@ import messages from "./messages.json";
 function ProductInfoSection() {
   return (
     <div className="vertical-flow-container">
-      <div className="imagesdiv">
-        <div className="onediv">
+      <div className="order-process-parent">
+        <div className="order-process-child">
           <img
             src="https://d2e8m995jm0i5z.cloudfront.net/websiteimages/img_1135_8915_714d59.svg"
             alt=""
             className="image-block"
           />
-          <h1>
-            1<div className="line"></div>
-          </h1>
+          <h1 className="containsline">1</h1>
           <p>Inquiry quotation</p>
         </div>
-        <div className="onediv">
+        <div className="order-process-child">
           <img
             src="https://d2e8m995jm0i5z.cloudfront.net/websiteimages/img_1091_3692_458f26.svg"
             alt=""
@@ -26,7 +24,7 @@ function ProductInfoSection() {
           <h1>2</h1>
           <p>Place your order</p>
         </div>
-        <div className="onediv">
+        <div className="order-process-child">
           <img
             src="https://d2e8m995jm0i5z.cloudfront.net/websiteimages/img_1135_8920_20e63d.svg"
             alt=""
@@ -35,7 +33,7 @@ function ProductInfoSection() {
           <h1>3</h1>
           <p>Manufacturing your product</p>
         </div>
-        <div className="onediv">
+        <div className="order-process-child">
           <img
             src="https://d2e8m995jm0i5z.cloudfront.net/websiteimages/img_1091_3700_db0891.svg"
             alt=""
@@ -44,7 +42,7 @@ function ProductInfoSection() {
           <h1>4</h1>
           <p>Inspection & packaging</p>
         </div>
-        <div className="onediv">
+        <div className="order-process-child">
           <img
             src="https://d2e8m995jm0i5z.cloudfront.net/websiteimages/img_1091_3702_cbd766.svg"
             alt=""
